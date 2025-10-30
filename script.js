54e0802103ce0b07891d2985c58cfd168fcc4b80
Raphael Moreira <43351851+RaphaelM20@users.noreply.github.com>
2025-10-30 14:28:46 -0400

    Add book creation and display logic
    Add form submission with validation and preventDefault()
    Render books as cards dynamically using displayLibrary()
    Include show/hide functionality for the add book form

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//constructor and functions for myLibrary
+
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -12,17 +14,49 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-function addBookToLibrary(title, author, pages, read) {
-    const newBook = new Book(title, author, pages, read);
+function addBookToLibrary() {
+    const bookTitle = document.getElementById('book_title').value;
+    const bookAuthor = document.getElementById('book_author').value;
+    const bookPages = document.getElementById('book_pages').value;
+    const readBook = document.getElementById('book_read').checked;
+    const newBook = new Book(bookTitle, bookAuthor, bookPages, readBook);
     myLibrary.push(newBook)
+
+    displayLibrary();
 }
 
 function displayLibrary() {
+    const cardsContainer = document.querySelector('.cards');
+    cardsContainer.innerHTML = '';
+
     for (const book of myLibrary) {
-        console.log(book)
+        const card = document.createElement('div');
+        card.classList.add('book-card');
+
+        card.innerHTML = `
+            <h3>${book.title}</h3>
+            <p><strong>Author:</strong> ${book.author}</p>
+            <p><strong>Pages:</strong> ${book.pages}</p>
+            <p><strong>Read:</strong> ${book.read ? '✅ Yes' : '❌ No'}</p>
+            `
+        cardsContainer.append(card);
     }
 }
 
-//constructor and functions for myLibrary
+//unhide form
 
-//const document.querySelector('.addBookButton')
+const bookButton = document.querySelector('#addBookButton')
+
+bookButton.addEventListener('click', () => {
+    document.getElementById('addBookForm').hidden = false;
+})
+
+//submit button to add and display library
+
+document.getElementById('addBookForm').addEventListener('submit', (event) => {
+    event.preventDefault();
+    addBookToLibrary();
+})
+
+
+
